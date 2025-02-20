62771078f4a76421d0fa676e448183c325bd3f2b
Javad Asgari Shafique <me@javad.sh>
2025-02-20 10:30:09 +0100

    Windows ;) (also don't run CI for this commit)

--- hyggec.bat
+++ hyggec.bat
@@ -1,2 +1,7 @@
 @echo off
-dotnet run -- %*
+for /f "tokens=1 delims=." %%a in ('dotnet --version') do set DOTNET_VERSION=%%a.%%b
+set FRAMEWORK_VERSION=net%DOTNET_VERSION%
+set ARGS=-v q --framework %FRAMEWORK_VERSION% -c Debug
+
+dotnet build %ARGS% > nul 2>&1
+dotnet run --no-build %ARGS% -- %*
