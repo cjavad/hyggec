94e79734ba782ca55de7973e7cb1aacd5b87e64c
Magnus <88041689+MagnusAH@users.noreply.github.com>
2025-02-27 09:22:59 +0100

    dotnet no longer complains
    i hate microsoft and everything they make

--- hyggec.bat
+++ hyggec.bat
@@ -1,7 +1,7 @@
 @echo off
 for /f "tokens=1,2 delims=." %%a in ('dotnet --version') do set DOTNET_VERSION=%%a.%%b
 set FRAMEWORK_VERSION=net%DOTNET_VERSION%
-set ARGS=-v q --framework %FRAMEWORK_VERSION% -c Debug
+set ARGS=-v q -c Debug
 
 dotnet build %ARGS%
 dotnet run --no-build %ARGS% -- %*
